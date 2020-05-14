f032e83988059abb0d0349061f81f9f8abf74c80
Depak Kumar <depakkumar31@gmail.com>
2020-05-14 18:35:54 -0400

    Updated Map and fonts
    Updated the map to look better on more devices. Made sure the app description would be more accurate on browser tabs. Changed to Nunito font use Google Fonts

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { HashRouter as Router } from 'react-router-dom';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
@@ -15,15 +16,29 @@ const styles = (theme) => ({
   },
 });
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Nunito Sans',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+    ].join(','),
+  },
+});
+
 const App = (props) => {
   const { classes } = props;
   return (
     <Router>
-      <Grid container component="main" className={classes.root}>
+      <MuiThemeProvider theme={theme}>
         <CssBaseline />
-        <Sidebar />
-        <Content />
-      </Grid>
+        <Grid container component="main" className={classes.root}>
+          <Sidebar />
+          <Content />
+        </Grid>
+      </MuiThemeProvider>
     </Router>
   );
 };
